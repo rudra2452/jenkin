68e18164f2a411755976848cf4de15502219c376
rudra2452 <prem1905@hotmail.com>
2024-10-04 22:07:13 +0530

    jenkin

---
+++ enkins.py
@@ -0,0 +1,1 @@
+print("Shankarji is with me")
---
+++ sum.py
@@ -0,0 +1,5 @@
+print("program to add 2 values")
+number1 = int(input("enter the number1:"))
+number2 =int(input("enter the number2:"))
+result = number1 + number2
+print(f'sum of 2 numbers are : {result}')
